bc005d06b4a474218b3f1e81ffb544a58d1c328f
maramivivo <393605@mail.ru>
2021-01-15 16:05:42 +0400

    Переработал код для прохода вглубь. Однако скрипт работает по поверхности дерева файлов, вглубь работает только в несколько слоёв

--- Test_rename/rename_file.py
+++ Test_rename/rename_file.py
@@ -1,20 +1,41 @@
 import os
-
-dirr = os.getcwd()
+'''
+По факту происходит переименование нескольких верхних уровней, в глубь скрипт не идёт (Почему!?)
+'''
 
 bad_list = ['[Отборные Сливы] ', '[MEGASLIV.BIZ] ', '[sharewood.band] ',
             '[sharewood.biz] ', '[share-wood.biz] ', '[slivoman.com] ',
             '[BOOMINFO.RU] ', '[Infosklad.org] ', '[sliwbl.biz] ',
             '[Boominfo.ORG] ', '[www.slifki.info] ', '[SW.BAND] ',
-            '[SuperSliv.BiZ] ', '[BOOMINFO.ORG] ']
+            '[SuperSliv.BiZ] ', '[BOOMINFO.ORG] ', '[Example] ']
+startdir = os.getcwd()
 
-bs = input('Ввести все символы, подлежащие удалению (включая пробелы)\n')
+def my_rename():
+    for directory, folder, file in os.walk(os.getcwd()): # начало цикла из текущей директории (где находится файл)
+        os.chdir(directory)
+        for f in file:
+            if file:
+                for i in bad_list:
+                    if i in f:
+                        new_f = f.replace(i, '')
+                        os.rename(f, new_f)
+        print('Переименование файлов завершено\n\n')
 
-files = os.listdir(dirr)
-for f in files:
-    if bs in f:
-        new_f = f.replace(bs, '')
-        os.rename(f, new_f)
+        for fold in folder:
+            if folder:
+                for i in bad_list:
+                    if i in fold:
+                        new_fold = fold.replace(i, '')
+                        try:
+                            os.rename(fold, new_fold)
+                        except:
+                            continue
+        print('Переименование папок завершено\n')
+        
+    os.chdir(startdir)
+            
 
-print(bad_list)
+my_rename()
+print('\n=========================================\n\nПереименование всех данных завершено')
+print(os.getcwd())
 input()
