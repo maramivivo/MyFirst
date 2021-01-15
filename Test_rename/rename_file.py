94f115ea2adb57c326cf61e7d6d2fe84bf2f023e
maramivivo <393605@mail.ru>
2021-01-15 09:58:18 +0400

    Fixed code

--- Test_rename/rename_file.py
+++ Test_rename/rename_file.py
@@ -1,15 +1,20 @@
-
 import os
 
 dirr = os.getcwd()
-##print(dirr)
 
-bs = input('Ввести символы, полдежащие удалению (включая пробелы)\n')
+bad_list = ['[Отборные Сливы] ', '[MEGASLIV.BIZ] ', '[sharewood.band] ',
+            '[sharewood.biz] ', '[share-wood.biz] ', '[slivoman.com] ',
+            '[BOOMINFO.RU] ', '[Infosklad.org] ', '[sliwbl.biz] ',
+            '[Boominfo.ORG] ', '[www.slifki.info] ', '[SW.BAND] ',
+            '[SuperSliv.BiZ] ', '[BOOMINFO.ORG] ']
+
+bs = input('Ввести все символы, подлежащие удалению (включая пробелы)\n')
+
 files = os.listdir(dirr)
 for f in files:
-##    print(f)
-##    print('==========\n')
     if bs in f:
         new_f = f.replace(bs, '')
         os.rename(f, new_f)
-##        print(new_f)
+
+print(bad_list)
+input()
