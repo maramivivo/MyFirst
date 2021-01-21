660b0bd89efa8054f0b1a3faec5223c3a8a84ce2
maramivivo <393605@mail.ru>
2021-01-21 09:42:29 +0400

    Fixed

--- Test_rename/rename_remove.py
+++ Test_rename/rename_remove.py
@@ -9,11 +9,13 @@ bad_list = ['[BOOMINFO.ORG] ', '[BOOMINFO.RU] ', '[Boominfo.ORG] ',
 # список названий файлов для удаления
 remove_list = ['MEGASLIV.BIZ - Качай курсы беслпатно!.url',
                'SHAREWOOD_ZERKALO_COM_90000_курсов_на_нашем_форуме!.url',
+               'SHAREWOOD_BIZ_Качай_популярные_курсы.url'
                '[www.slifki.info]Спасибо за загрузку!.docx',
                '[MEGASLIV.BIZ] Обязательно к прочтению.docx',
                '[SW.BAND] Прочти перед изучением!.docx',
-               '[slivoman.com] До изучения.docx',
-               '[infobiza.net] Информация.docx'] 
+               '[SW.BAND] Прочти перед изучением! .docx',
+               '[slivoman.com] До изучения.docx',]
+CHEK_LIST = ['[sharewood.biz] Информация.docx', '[infobiza.net] Информация.docx']
 
 def remove_files():
     def foo_remove(files):
@@ -29,14 +31,14 @@ def remove_files():
     for directory, folders, files in reverse_walk:
         os.chdir(directory)
         foo_remove(files)
-    print('>>> Лишние файлы удалены\n')
+    print('\n>>> Лишние файлы удалены')
 
 
 def rename():
     def foo_rename(files_folders):
         for name in files_folders:
             for el in bad_list:
-                if el in name:
+                if el in name and name not in CHEK_LIST:
                     new_name = name.replace(el, '')
                     try:
                         os.rename(name, new_name)
@@ -45,14 +47,31 @@ def rename():
                         print(traceback.print_exc(limit=0))
                     except PermissionError:
                         print(traceback.print_exc(limit=0))
-
     reverse_walk = list(os.walk(os.getcwd()))[::-1]
     for directory, folders, files in reverse_walk:
         os.chdir(directory)
-        foo_rename(folders)
         foo_rename(files)
+        foo_rename(folders)
     print('\n>>> Все папки и файлы переименованы')
-    
+
+
+def chek():
+    print_chek = []
+    def foo_chek(files_folders):
+        for el in CHEK_LIST:
+            for i in files_folders:
+                if el == i:
+                    if i not in print_chek:
+                        print_chek.append(i)
+##            print(files_folders)
+    reverse_walk = list(os.walk(os.getcwd()))[::-1]
+    for directory, folders, files in reverse_walk:
+        os.chdir(directory)
+        foo_chek(files)
+    print('>>> Необходимо проверить следующие файлы (удалить?):')
+    for i in print_chek:
+        print('|',i)
+
 print('\nИз названий папок и файлов будут удалены следующие сочатния символов:')
 for i in bad_list:
     print('->', i)
@@ -62,9 +81,11 @@ for i in remove_list:
     print('->', i)
 input('\nНажмите "Enter" для запуска файла\n')
 
+chek()
 remove_files()
 rename()
+
 input('\nНажмите "Enter" для выхода')
 
-if __name__ == "__main__":
-    main()
+##if __name__ == "__main__":
+##    main()
