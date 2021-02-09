27d8dab19c20617b612d9e1a21f033ef663d609b
maramivivo <393605@mail.ru>
2021-02-09 16:39:25 +0400

    ...Формирование word-документов. Исправления, доработки...

--- Формирование word-документов/word.py
+++ Формирование word-документов/word.py
@@ -1,6 +1,7 @@
 import openpyxl
 from docxtpl import DocxTemplate
 
+
 ##input('Нажмите "Enter" для запуска скрипта\n')
 
 wb = openpyxl.load_workbook('Данные\\Данные.xlsx')
@@ -13,17 +14,20 @@ if '\\' in save_location:
 
 for i in range(n):
     if i < (n-1):
-        company = sheet['A'+ str(i + 2)].value
-        adress = sheet['B'+ str(i +2)].value
-        post = sheet['C'+ str(i +2)].value
-        short_director = sheet['D'+ str(i +2)].value
-        initials = sheet['E'+ str(i +2)].value
+        company = sheet['A' + str(i + 2)].value
+        adress = sheet['B' + str(i + 2)].value
+        post = sheet['C' + str(i + 2)].value
+        short_director = sheet['D' + str(i + 2)].value
+        initials = sheet['E' + str(i +2 )].value
         doc = DocxTemplate('Данные\\Письмо_шаблон.docx')
         context = {'company': company, 'adress': adress, 'post': post,
                    'short_director': short_director, 'initials': initials}
         doc.render(context)
-        save_location_end = save_location + f'\\Письмо_{i+1}.docx'
-        doc.save(save_location_end)
-        print(f'Письмо_{i+1}.docx') 
+        file = sheet['F' + str(i + 2)].value
+        save_location_word = save_location + f'\\Письмо _{file}.docx'
+        doc.save(save_location_word)
+        
+##        print(f'Письмо _{file}.docx')
+        break
 
 input('Нажмите "Enter" для выхода')
